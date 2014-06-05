e4544b3a93a1d8aa813a258bb67776998f95aeb1
marto00 <martin14071@abv.bg>
2014-06-05 16:25:39 +0300

    Create manapool.h

---
+++ manapool.h
@@ -0,0 +1,11 @@
+#ifndef MANAPOOL_H
+#define MANAPOOL_H
+#include "card.h"
+        typedef struct{
+        int max_mana;
+        int current_mana;
+    }manapool_t;
+    int can_put_card(card_t card_to_put, manapool_t *pool);
+    void add_mana(manapool_t *pool, int mana_to_add);
+    void remove_mana(manapool_t *pool, int mana_to_add);
+#endif /* MANAPOOL_H */
